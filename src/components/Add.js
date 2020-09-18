a5447cea57b9d7234485e85aabe0efe9c17bcc8d
Elena Mylläri <elena.myllari@metropolia.fi>
2020-09-18 22:53:18 +0300

    error and success messages

--- src/components/Add.js
+++ src/components/Add.js
@@ -6,31 +6,40 @@ import 'react-datepicker/dist/react-datepicker.css'
 
 const Add =()=>  {
 
-    const date = moment().toDate()
+    const date = moment().toDate();
 
-    const [newCourse, setNewCourse] = useState('')
-    const [newTask, setNewTask] = useState('')
-    const [newDone, setNewDone] = useState(false)
-    const [newDeadline, setNewDeadline] = useState(date)
+    const [newCourse, setNewCourse] = useState('');
+    const [newTask, setNewTask] = useState('');
+    const [newDone, setNewDone] = useState(false);
+    const [newDeadline, setNewDeadline] = useState(date);
+    const [error, setError] = useState(false);
+    const [success, setSuccess] = useState(false);
 
     const handleSubmit = (event) => {
-        event.preventDefault()
-        const id = getId();
-
-        const noteObject = {
-            course: newCourse,
-            task: newTask,
-            done: newDone,
-            date: newDeadline.toLocaleDateString(),
-            id: id,
+        event.preventDefault();
+        if(newCourse.length===0 || newTask.length===0){
+            setError(true)
         }
-        noteService.create(noteObject).then(res=>{
-            setNewCourse('')
-            setNewTask('')
-            setNewDone(false)
-            setNewDeadline(date)
-        })
+        else {
+            setError(false);
+            setSuccess(true);
+            console.log(success)
+            const id = getId();
 
+            const noteObject = {
+                course: newCourse,
+                task: newTask,
+                done: newDone,
+                date: newDeadline.toLocaleDateString(),
+                id: id,
+            };
+            noteService.create(noteObject).then(res => {
+                setNewCourse('');
+                setNewTask('');
+                setNewDone(false);
+                setNewDeadline(date)
+            })
+        }
     };
 
 
@@ -38,23 +47,39 @@ const Add =()=>  {
         noteService.getAll().then(res=>{
             return res.length+1
         });
-    }
+    };
 
     const handleCourseChange=(event)=> {
         setNewCourse(event.target.value)
-    }
+    };
 
     const handleTaskChange= (event)=> {
         setNewTask(event.target.value)
-    }
+    };
 
     const handleDoneChange = (event) => {
         setNewDone(Boolean(event.target.value))
-    }
+    };
 
     const handleDeadlineChange = (value) => {
         setNewDeadline(value)
-    }
+    };
+
+    const errorCourse=()=>{
+        return newCourse.length === 0 && error===true
+    };
+
+    const errorTask =() => {
+        return newTask.length === 0 && error===true
+    };
+
+    const errorStyle ={
+        color: 'red'
+    };
+
+    const successStyle ={
+        color: 'green'
+    };
 
     return (
 
@@ -64,9 +89,11 @@ const Add =()=>  {
                         <label>Course
                             <input name="course" value={newCourse}  onChange={handleCourseChange}/>
                         </label>
+                        {errorCourse() && <p style={errorStyle}>Fill in the course name, please</p>}
                         <label> Task
                             <input name="task"  value={newTask} onChange={handleTaskChange}/>
                         </label>
+                        {errorTask() && <p style={errorStyle}>Fill in the task, please</p>}
                         <label>Deadline
                             <DatePicker
                                 name="deadline"
@@ -87,11 +114,12 @@ const Add =()=>  {
                         </label>
                         <button type="submit">Save</button>
                     </form>
+                    {success && <p style={successStyle}>Data saved successfully!</p>}
                 </div>
             </div>
         )
 
-}
+};
 
 
 export default Add;
